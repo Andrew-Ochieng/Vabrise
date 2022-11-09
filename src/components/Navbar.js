59ca3b3b6002fc8d2054f11285da4b5bdbc65640
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-11-09 16:54:06 +0300

    initial commit && navbar setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import { useRef } from "react";
+
+const Navbar = () => {
+    const menuRef = useRef()
+
+    const showMenu = () => {
+        menuRef.current.classList.toggle("hidden")
+    }
+
+    return ( 
+        <div className="">
+            <nav className='md:py-6 py-4 px-4'>
+                <div className="navbar md:flex  justify-around items-center " >
+                    <div className='flex items-center justify-between'>
+                        <Link to='/' className='md:text-3xl text-2xl uppercase font-semibold '>Vabrise Technologies</Link>
+                        <button onClick={showMenu} className="md:hidden">
+                            <i className="text-xl fa fa-bars"></i> 
+                            {/* <i className="fa fa-times"></i> */}
+                        </button>
+                    </div>
+
+                    <div ref={menuRef} className="md:flex hidden ">
+                        <ul className="md:flex items-center md:text-lg font-medium">
+                            <li className='md:mx-8 md:my-0 my-2'>
+                                <Link to='/'>Home</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+     );
+}
+ 
+export default Navbar;
