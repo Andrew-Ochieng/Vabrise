28f2455c3893c6ebc11224465ad34c2565e1b8b7
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-13 11:24:34 +0300

    add change from npm to yarn

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,19 +13,34 @@ const Navbar = () => {
     return ( 
         <div className="">
             <nav className='md:py-6 py-4'>
-                <div className="navbar md:flex  justify-between items-center " >
+                <div className="navbar justify-between items-center " >
                     <div className='flex items-center justify-between'>
                         <Link to='/' className='md:text-3xl text-2xl uppercase font-semibold '>
                             <img className="md:w-32 w-28" src={logo} alt="Logo.." />
                         </Link>
+                        
+                        <ul className="md:flex items-center md:text-lg font-medium hidden">
+                            <li className='md:mx-8 md:my-0 my-2'>
+                                <Link to='/about'>About</Link>
+                            </li>
+                            <li className='md:mx-8 md:my-0 my-2'>
+                                <Link to='/services'>Services</Link>
+                            </li>
+                            <li className='md:mx-8 md:my-0 my-2'>
+                                <Link to='/blog'>Blog</Link>
+                            </li>
+                            <li className='md:mx-8 md:my-0 my-2'>
+                                <Link className="border-2 bg-green-400 px-4 py-1 hover:bg-green-600 duration-500 hover:text-white rounded-md" to='/contact'>Contact</Link>
+                            </li>
+                        </ul>
+
                         <button onClick={showMenu} className="md:hidden">
                             <FaBars />
-                            {/* <i className="fa fa-times"></i> */}
                         </button>
                     </div>
 
-                    <div ref={menuRef} className="md:flex hidden ">
-                        <ul className="md:flex items-center md:text-lg font-medium">
+                    <div ref={menuRef} className="md:hidden block">
+                        <ul className="md:text-lg font-medium">
                             <li className='md:mx-8 md:my-0 my-2'>
                                 <Link to='/team'>Team</Link>
                             </li>
