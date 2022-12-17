2ac94edffc39244736a5c0ca23d5e005e8fe57aa
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-17 09:36:38 +0300

    add service image links

--- src/components/ServiceItem.js
+++ src/components/ServiceItem.js
@@ -1,18 +1,14 @@
-import Services from "../data.json"
+import services from "../data.json"
 
 const ServiceItem = () => {
     return ( 
         <>
-            <div className="md:mt-4 mt-2 md:mb-8 mb-4">
-                    <h2 className="text-red-600 text-xl">Work in progress here...</h2>
-            </div>
-
             <div className="grid sm:grid-cols-2 md:grid-cols-3 md:gap-16 gap-8">
-                {Services.map((service) => (
-                    <div className="" key={service.image}>
-                        {/* <img src={service.image} alt=""> */}
-                        <h1 className="md:text-3xl text-xl font-semibold text-gray-800">{service.title}</h1>
-                        <h3 className="md:text-xl text-lg my-1 text-green-500">{service.subtitle}</h3>
+                {services.map((service) => (
+                    <div className="" key={service.id}>
+                        <img src={service.path} alt="services" className="w-full"/>
+                        <h1 className="md:text-3xl text-xl font-semibold text-gray-800 pt-6">{service.title}</h1>
+                        <h3 className="md:text-xl text-lg my-1 text-green-900">{service.subtitle.slice(0, 30)}</h3>
                         <p className="md:text-lg">{service.content.split(/\s+/).slice(0, 20).join(" ")}</p>
                     </div>
                 ))}
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,7 +3,7 @@ import ServiceItem from "../components/ServiceItem";
 const Services = () => {
     return ( 
         <>
-            <div className="md:mt-4 mt-2 md:mb-64 mb-32">   
+            <div className="md:mt-8 mt-4 md:mb-64 mb-32">   
                 <ServiceItem />
             </div>
         </>
