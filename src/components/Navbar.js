3c3ddc7eae32a1c018394d5659a109c62c5adc67
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-21 11:23:06 +0300

    cleanup menu links code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,54 +10,39 @@ const Navbar = () => {
         menuRef.current.classList.toggle("hidden")
     }
 
+    let links = [
+        {name: "About", link: "/about"},
+        {name: "Services", link: "/services"},
+        {name: "Blog", link: "/blog"},
+        {name: "Contact", link: "/contact"}
+    ]
+
     return ( 
-        <div className="">
+        <>
             <nav className='md:py-6 py-4'>
-                <div className="navbar justify-between items-center " >
+                <div className="navbar md:flex justify-between items-center " >
                     <div className='flex items-center justify-between'>
                         <Link to='/' className='md:text-3xl text-2xl uppercase font-semibold '>
                             <img className="md:w-32 w-28" src={logo} alt="Logo.." />
                         </Link>
-                        
-                        <ul className="md:flex items-center md:text-lg font-medium hidden">
-                            <li className='md:mx-8 md:my-0 my-2'>
-                                <Link to='/about'>About</Link>
-                            </li>
-                            <li className='md:mx-8 md:my-0 my-2'>
-                                <Link to='/services'>Services</Link>
-                            </li>
-                            <li className='md:mx-8 md:my-0 my-2'>
-                                <Link to='/blog'>Blog</Link>
-                            </li>
-                            <li className='md:mx-8 md:my-0 my-2'>
-                                <Link className="border-2 bg-green-400 px-4 py-1 hover:bg-green-600 duration-500 hover:text-white rounded-md" to='/contact'>Contact</Link>
-                            </li>
-                        </ul>
 
                         <button onClick={showMenu} className="md:hidden">
                             <FaBars />
                         </button>
                     </div>
 
-                    <div ref={menuRef} className="md:hidden block">
-                        <ul className="md:text-lg font-medium">
-                            <li className='md:mx-8 md:my-0 my-2'>
-                                <Link to='/about'>About</Link>
-                            </li>
-                            <li className='md:mx-8 md:my-0 my-2'>
-                                <Link to='/services'>Services</Link>
-                            </li>
-                            <li className='md:mx-8 md:my-0 my-2'>
-                                <Link to='/blog'>Blog</Link>
-                            </li>
-                            <li className='md:mx-8 md:my-0 my-2'>
-                                <Link className="border-2 bg-green-400 px-4 py-1 hover:bg-green-600 duration-500 hover:text-white rounded-md" to='/contact'>Contact</Link>
-                            </li>
+                    <div ref={menuRef} className="md:flex hidden ">
+                        <ul className="md:flex items-center md:text-lg font-medium">
+                            {links.map((link) => (
+                                <li className='md:mx-8 md:my-0 my-2'>
+                                    <Link to={link.link}>{link.name}</Link>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
             </nav>
-        </div>
+        </>
     );
 }
  
