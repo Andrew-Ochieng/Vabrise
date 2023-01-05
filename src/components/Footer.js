887465c2db61109ad2ab2f1a67e99b6b231ea686
Andrew-Ochieng <andrewomosh44@gmail.com>
2023-01-05 17:54:37 +0300

    add scroll-to-top button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
-import { BsGithub, BsInstagram, BsTwitter, BsLinkedin, BsFacebook } from "react-icons/bs"
-
+import { BsGithub, BsInstagram, BsTwitter, BsLinkedin, BsFacebook, BsArrowUpSquareFill } from "react-icons/bs"
+import { motion } from "framer-motion";
 
 const Footer = () => {
     const today = new Date()
     const newYear = today.getFullYear()
     // console.log(newYear)
+
+    const scrollTop = () => {
+        window.scrollTo(0, 0);
+    }
 
     let links = [
         {name: "About", link: "/about"},
@@ -32,23 +36,36 @@ const Footer = () => {
                         </ul>
                     </div>
 
-                    <div className="flex justify-center text-lg text-gray-900">
-                        <Link to='/'  className="mx-2">
-                            <BsGithub />
-                        </Link>
-                        <Link to='/'  className="mx-2">
-                            <BsFacebook />
-                        </Link>
-                        <Link to='/'  className="mx-2">
-                            <BsInstagram />
-                        </Link>
-                        <Link to='/'  className="mx-2">
-                            <BsTwitter />
-                        </Link>
-                        <Link to='/'  className="mx-2">
-                            <BsLinkedin />
-                        </Link>
+                    <div className="flex items-center justify-center">
+                        <div className="flex justify-center text-lg text-gray-900">
+                            <Link to='/'  className="mx-2">
+                                <BsGithub />
+                            </Link>
+                            <Link to='/'  className="mx-2">
+                                <BsFacebook />
+                            </Link>
+                            <Link to='/'  className="mx-2">
+                                <BsInstagram />
+                            </Link>
+                            <Link to='/'  className="mx-2">
+                                <BsTwitter />
+                            </Link>
+                            <Link to='/'  className="mx-2">
+                                <BsLinkedin />
+                            </Link>
+                        </div>
+
+                        {/* scroll to top */}
+                        <motion.button 
+                            initial={{x: '100vh'}}
+                            animate={{x: 0}}
+                            whileHover={{scale: 1.4}}
+                            onClick={scrollTop} 
+                            className="md:text-3xl text-2xl ml-8 text-right text-green-500 hover:text-green-600 shadow-lg hover:shadow-xl shadow-blue-300 animate-bounce">
+                            <BsArrowUpSquareFill />
+                        </motion.button>
                     </div>
+
                     
                 </div>
                 <div className="flex justify-center items-center text-sm">
--- src/components/OurClients.js
+++ src/components/OurClients.js
@@ -1,7 +1,12 @@
 const OurClients = () => {
+    
     return ( 
         <>
-            Our Clients here...
+            <div className="flex flex-col items-center justify-center pt-8 pb-4 md:text-3xl text-xl text-gray-800 font-semibold">
+                <h1 >Our Clients</h1>
+                <hr className="border-2 border-green-400 mt-2 md:w-32 w-24"/>
+            </div>
+
         </>
      );
 }
