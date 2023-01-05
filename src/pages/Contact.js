45cbc66fe5e47f26148aaed8c0b84a9bdd7de0b2
Andrew-Ochieng <andrewomosh44@gmail.com>
2023-01-05 19:44:14 +0300

    add page header

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,20 +6,24 @@ import { HiOutlineMail } from "react-icons/hi";
 const Contact = () => {
     return ( 
         <>
+            <div className="flex flex-col items-center justify-center pt-8 pb-4 md:text-3xl text-xl text-gray-800 font-semibold">
+                <h1 >Reach Out</h1>
+                <hr className="border-2 border-green-400 mt-2 md:w-32 w-24"/>
+            </div>
             <div className="md:flex justify-around md:my-16 my-8 md:pb-24 pb-0">
                 <div>
                     <div class="flex flex-col my-8 md:text-2xl text-center text-2xl text-gray-700 space-y-8 ">
                         <div class="flex flex-col justify-center items-center rounded-lg bg-blue-300 p-8 shadow-lg hover:shadow-xl">
                             <FaPhoneAlt />
-                            <p class="md:ml-4 ml-2 mt-4 md:text-xl text-lg">0799874578/ 0710815080</p>
+                            <p class="md:ml-4 ml-2 mt-2 md:text-xl text-lg">0799874578/ 0710815080</p>
                         </div>
                         <div class="flex flex-col justify-center items-center rounded-lg bg-blue-300 p-8 shadow-lg hover:shadow-xl">
                             <MdLocationOn />
-                            <p class="md:ml-4 ml-2 mt-4 md:text-xl text-lg">Nairobi, Kenya</p>
+                            <p class="md:ml-4 ml-2 mt-2 md:text-xl text-lg">Nairobi, Kenya</p>
                         </div>
                         <div class="flex flex-col justify-center items-center rounded-lg bg-blue-300 p-8 shadow-lg hover:shadow-xl">
                             <HiOutlineMail />
-                            <p class="md:ml-4 ml-2 mt-4 md:text-xl text-lg">info@vabrise.com</p>
+                            <p class="md:ml-4 ml-2 mt-2 md:text-xl text-lg">info@vabrise.com</p>
                         </div>
                     </div>
                 </div>
